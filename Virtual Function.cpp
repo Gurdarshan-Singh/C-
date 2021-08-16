5f9c6dcd55d96e924fd3b8d776c0fc44e7a4a7b7
Gurdarshan Singh <79242952+Gurdarshan-Singh@users.noreply.github.com>
2021-08-16 20:28:48 +0530

    Create Virtual Function.cpp

---
+++ Virtual Function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class base{
+    public:
+    int var_base=10;
+    virtual void display(){
+        cout<<"The value of the base var is "<<var_base<<endl;
+    }
+};
+
+class derived:public base{
+    public:
+    int var_der=20;
+    void display(){
+        cout<<"The value of the base var is "<<var_base<<endl;
+        cout<<"The value of the derived var is "<<var_der<<endl;
+        
+    }
+};
+int main(){
+    base *bptr;
+    base objb;
+    derived objd;
+    bptr=&objd;
+    bptr->display();
+    
+return 0;
+}
